9233f0033e3fdd1b5a112131ce68dc81221bfc99
cheng'feng <1300749625@qq.com>
2021-03-14 22:05:35 +0800

    study：基础代码编写

--- src/views/study-dome/index.vue
+++ src/views/study-dome/index.vue
@@ -370,6 +370,38 @@ export default {
   },
   mounted() {
     // window.onscroll = debounce(this.getMouseXY, 1000)
+    const list = [
+      { id: 1, name: '部门A', parentId: 0 },
+      { id: 2, name: '部门B', parentId: 0 },
+      { id: 3, name: '部门C', parentId: 1 },
+      { id: 4, name: '部门D', parentId: 1 },
+      { id: 5, name: '部门E', parentId: 2 },
+      { id: 6, name: '部门F', parentId: 3 },
+      { id: 7, name: '部门G', parentId: 2 },
+      { id: 8, name: '部门H', parentId: 4 }
+    ]
+
+    function convert(list) {
+      const result = []
+      const map = {}
+      list.forEach(item => {
+        if (item.parentId === 0) {
+          result.push(item)
+        }
+        map[item.id] = item
+      })
+
+      list.forEach(item => {
+        if (item.parentId !== 0) {
+          const parent = map[item.parentId]
+          parent.children = parent.children || []
+          parent.children.push(item)
+        }
+      })
+      console.log(result)
+      return result
+    }
+    convert(list)
   },
   methods: {
     goback() {
